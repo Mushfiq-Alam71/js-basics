0426d2426ef92b07cf003c853e47bbe0bf532770
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2024-01-31 00:51:25 +0600

    task 4 of array done!

---
+++ array-task4.js
@@ -0,0 +1,5 @@
+let studentName = ["jasim", "karim", "mosim", "posim", "tosim"];
+let studentMarks = "this is not student marks";
+
+console.log(Array.isArray(studentName));
+console.log(Array.isArray(studentMarks));

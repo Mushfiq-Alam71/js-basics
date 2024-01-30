6651785f09245fcba0fb629561abb1180e66ab70
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2024-01-30 22:10:13 +0600

    task 2 of array done!

---
+++ array-task2.js
@@ -0,0 +1,11 @@
+let travelSpot = ["cox's bazar", "saint martin", "rangamati"];
+console.log(travelSpot);
+
+travelSpot.push("bandarban");
+console.log(travelSpot);
+
+travelSpot.push("sadapathor", "kuakata");
+console.log(travelSpot);
+
+travelSpot.pop();
+console.log(travelSpot);

c0c5937e19a0144d9791799909a6a6bca3018524
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2024-02-02 00:51:49 +0600

    array task 5 done

---
+++ array-task5.js
@@ -0,0 +1,6 @@
+let studentName = ["jasim", "karim", "mosim", "posim", "tosim"];
+let studentMarks = [78, 87, 93, 90, 84];
+
+let studentResult = studentName.concat(studentMarks);
+
+console.log(studentResult);

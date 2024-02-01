d651a275d0540086df5b8cde49860f93138b9634
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2024-02-02 01:32:01 +0600

    while loop task 2

---
+++ whileloop-task2.js
@@ -0,0 +1,17 @@
+// For Even Number
+let x = 61;
+while (x <= 100) {
+  if (x % 2 == 0) {
+    console.log(x);
+  }
+  x++;
+}
+
+// For Odd Number
+let y = 78;
+while (y <= 98) {
+  if (y % 2 !== 0) {
+    console.log(y);
+  }
+  y++;
+}

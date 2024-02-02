198e87165054bc867e3fa33ec148b2ac599333aa
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2024-02-02 16:32:02 +0600

    completed break task

---
+++ break-task3.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  // Check if the current number is a square number
+  if (Math.sqrt(i) % 1 === 0) {
+    console.log("Found the first square number (${i})! Exiting the loop.");
+    break;
+  }
+}

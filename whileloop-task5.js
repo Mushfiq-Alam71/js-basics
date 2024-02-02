1aaebdc58552a4e44d855fa6158079216a379d50
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2024-02-02 11:52:06 +0600

    while loop task 5 done

---
+++ whileloop-task5.js
@@ -0,0 +1,11 @@
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+let countDown = 21;
+while (countDown >= 15) {
+  console.log("countDown: " + countDown);
+  countDown--;
+}
